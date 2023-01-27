f62c46da9e0d3d50c89c597177e47b7fca53a793
John Doe <leehy0782@naver.com>
2023-01-27 18:56:38 +0900

    feat: Add Event Listener

--- client/src/App.js
+++ client/src/App.js
@@ -5,17 +5,24 @@ function App() {
   const socket = new WebSocket("ws://localhost:5000");
   // const socket = new WebSocket(`ws://${window.locati}`);
 
+  // connection이 open 되었을 때 실행될 이벤트
   socket.addEventListener("open", () => {
     console.log("서버와 성공적으로 연결됨!");
   });
 
+  // 서버로부터 메시지를 받았을 때 실행될 이벤트
   socket.addEventListener("message", (message) => {
     console.log("서버로부터 받아온 메세지임", message.data);
   });
 
+  // 서버가 오프라인이 되었을 때 실행될 이벤트
   socket.addEventListener("close", () => {
     console.log("서버와 연결끊김");
   });
+
+  setTimeout(() => {
+    socket.send("안녕 나는 브라우저야!");
+  }, 3000);
 
   return <p>안녕!</p>;
 }
--- server/server.js
+++ server/server.js
@@ -22,7 +22,20 @@ const wss = new WebSocket.Server({ server });
 wss.on("connection", (socket) => {
   // socket의 메서드, socket으로의 직접적인 연결을 제공함
   console.log("브라우저와 성공적으로 연결됨!");
-  socket.send("hello world!");
+
+  // 브라우저와의 연결이 종료됐을때
+  socket.on("close", () => {
+    console.log("브라우저와 연결끊김 ㅠㅠ");
+  });
+
+  // 메시지 전송: 브라우저 to 서버
+  socket.on("message", (message) => {
+    console.log(33);
+    console.log(message.toString("utf8"));
+  });
+
+  // 메시지 전송: 서버 to 브라우저
+  socket.send("안녕 친구들!");
 });
 
 server.listen(5000, () => {
